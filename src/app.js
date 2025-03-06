6f3e1a5732461b7350ce017084587e5c14f44b35
Kima93 <154838127+Kima93@users.noreply.github.com>
2025-03-06 19:47:32 +0000

    cambios1

--- src/app.js
+++ src/app.js
@@ -5,7 +5,53 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generarCartaAleatoria() {
+  const palos = [
+    { nombre: "Corazones", icono: "♥", color: "red" },
+    { nombre: "Picas", icono: "♠", color: "black" },
+    { nombre: "Tréboles", icono: "♣", color: "black" },
+    { nombre: "Diamantes", icono: "♦", color: "red" },
+  ];
+  const valores = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+  ];
+
+  const paloAleatorio = palos[Math.floor(Math.random() * palos.length)];
+  const valorAleatorio = valores[Math.floor(Math.random() * valores.length)];
+
+  return {
+    valor: valorAleatorio,
+    icono: paloAleatorio.icono,
+    color: paloAleatorio.color,
+  };
+}
+
+function mostrarCarta() {
+  const cartaGenerada = generarCartaAleatoria();
+  const topSuitIcon = document.getElementById("top-suit").querySelector("span");
+  const cardValue = document.getElementById("card-value").querySelector("h1");
+  const bottomSuitIcon = document.getElementById("bottom-suit").querySelector("span");
+
+  topSuitIcon.textContent = cartaGenerada.icono;
+  cardValue.textContent = cartaGenerada.valor;
+  bottomSuitIcon.textContent = cartaGenerada.icono;
+
+  topSuitIcon.style.color = cartaGenerada.color;
+  cardValue.style.color = cartaGenerada.color;
+  bottomSuitIcon.style.color = cartaGenerada.color;
+}
+
+// Llama a mostrarCarta() cada vez que se actualiza el sitio web
+mostrarCarta();
